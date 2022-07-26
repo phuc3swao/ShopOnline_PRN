83f3d68c1f0d4e60e0e226bafd50fd90a1c79df3
phuclh <hongphuc3301@gmail.com>
2022-07-26 23:23:42 +0700

    Add connection String to db in json file

--- OnlineShopPRN/OnlineShopG5/Models/ShopPRNContext.cs
+++ OnlineShopPRN/OnlineShopG5/Models/ShopPRNContext.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.Extensions.Configuration;
 
 #nullable disable
 
@@ -28,8 +29,10 @@ namespace OnlineShopG5.Models
         {
             if (!optionsBuilder.IsConfigured)
             {
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-                optionsBuilder.UseSqlServer("server=localhost; database=ShopPRN;user=sa;password=123");
+                var config = new ConfigurationBuilder()
+                    .AddJsonFile("appsettings.json")
+                    .Build();
+                optionsBuilder.UseSqlServer(config.GetConnectionString("DbConnection"));
             }
         }
 
